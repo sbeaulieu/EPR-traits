6fc225845583939f05bd5828f3983857b52e5f11
Stace Beaulieu <stace@whoi.edu>
2025-04-08 21:51:46 -0400

    rename event columns to DwC

--- EPR_pumps_BCODMO_OBIS.R
+++ EPR_pumps_BCODMO_OBIS.R
@@ -1,6 +1,6 @@
 #EPR_pumps_BCODMO_OBIS
 #Stace Beaulieu
-#2025-04-07
+#2025-04-08
 
 # R script to standardize EPR pumps Composite data to Darwin Core
 # and output tables for BCO-DMO (occurrence table with occurrences left and events right)
@@ -24,19 +24,25 @@ counts_input <- select(counts_input,-"...1")
 
 # initiate event table with top rows Composite sheet
 # ultimately for BCO-DMO join separately to counts_long using eventID
-event_metadata <- dplyr::slice_head(counts_input,n = 9)
+event_metadata <- dplyr::slice_head(counts_input,n = 8)
 
 # transpose for event table
 event_t <- data.table::transpose(event_metadata, keep.names = "eventID", make.names="All near bottom, including off-axis")
 
+# rename columns to Darwin Core
+event_dwc <- event_t %>%
+  rename(verbatimEventDate = "Date Deployed") %>%
+  rename(locality = "Location") %>%
+  rename(sampleSizeValue = "Liters Pumped")
+event_dwc$sampleSizeUnit = "litre"
+event_dwc$"Distance off axis in meters" <- as.integer(event_dwc$"Distance off axis in meters")
+event_dwc$"Distance off site in meters" <- as.integer(event_dwc$"Distance off site in meters")
+event_dwc <- event_dwc %>%
+  unite(locationRemarks, c("Distance off axis in meters", "Distance off site in meters", "Direction off axis or off site"), remove = FALSE)
 
-# "Date Deployed"
-# "Location"
+# not DwC
 # "Height above bottom in meters"
-# "Distance off axis in meters"
-# "Distance off site in meters"
-# "Direction off axis or off site"
-# "Liters Pumped"
+# "Cruise number"
 
 # initiate occurrence table
 counts <- slice(counts_input, 10:n())
